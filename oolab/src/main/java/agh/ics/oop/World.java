32f318345d9d85af78b7608edff0d34571aefc72
Konrad <karmatys888@gmail.com>
2023-10-20 12:14:08 +0200

    Added MapDirection class
    By accident commited on lab1 branch

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,15 +2,16 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.MapDirection;
 
 
 public class World {
     public static void main(String[] args) {
-        Vector2d position1 = new Vector2d(1, 2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2, 1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
+        MapDirection direct = MapDirection.NORTH;
+        System.out.println(direct);
+        System.out.println(direct.next());
+        System.out.println(direct.previous());
+        System.out.println(direct.toUnitVector());
     }
 
     public static void run(MoveDirection[] args) {
---
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -0,0 +1,36 @@
+package agh.ics.oop.model;
+
+public enum MapDirection {
+    NORTH("północ"),
+    EAST("wschód"),
+    SOUTH("południe"),
+    WEST("zachód");
+
+    private final String value;
+    private static final MapDirection[] vals = values();
+
+    MapDirection(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
+
+    public MapDirection next() {
+        return vals[(this.ordinal() + 1) % vals.length];
+    }
+
+    public MapDirection previous() {
+        return vals[(this.ordinal() - 1 + vals.length) % vals.length];
+    }
+
+    public Vector2d toUnitVector() {
+        return switch(this) {
+            case NORTH -> new Vector2d(0, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH -> new Vector2d(0, -1);
+            case WEST -> new Vector2d(-1, 0);
+        };
+    }
+}
