6d124e34ad33468990aa02af672d5881417177c5
Konrad <karmatys888@gmail.com>
2024-01-12 10:39:09 +0100

    Optimized the code

--- oolab/src/main/java/agh/ics/oop/presenter/WorldElementBox.java
+++ oolab/src/main/java/agh/ics/oop/presenter/WorldElementBox.java
@@ -3,32 +3,45 @@ package agh.ics.oop.presenter;
 import agh.ics.oop.model.WorldElement;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class WorldElementBox extends VBox {
     private static final int IMAGE_HEIGHT = 20;
     private static final int  IMAGE_WIDTH = 20;
+    private static  final Map<String, Image> images = new HashMap<>();
 
-    private String lastImage;
+    static {
+        String[] images = {"up.png", "right.png", "down.png", "left.png", "grass.png"};
+        for (String url : images) {
+            WorldElementBox.images.put(url, new Image(url));
+        }
+    }
+
+    private ImageView lastImageView;
+    private String lastImageUrl;
     private String lastLabel;
 
     // if simulation length was larger we could keep all 4 images for every animal
     // by doing so we would always create exactly 4 images per Animal (still one per Grass)
     // I believe that currently we will on average have less than 4
     private void updateImage(WorldElement element) {
-        String currImage = element.getImageResource();
+        String currImageUrl = element.getImageResource();
+        if (! Objects.equals(lastImageUrl, currImageUrl)) {
+            ImageView currImageView = new ImageView(images.get(currImageUrl));
+            currImageView.setFitHeight(IMAGE_HEIGHT);
+            currImageView.setFitWidth(IMAGE_WIDTH);
 
-        if (! Objects.equals(currImage, lastImage)) {
-            ImageView imageView = new ImageView(currImage);
-            imageView.setFitHeight(IMAGE_HEIGHT);
-            imageView.setFitWidth(IMAGE_WIDTH);
+            this.getChildren().remove(lastImageView);
+            this.getChildren().add(currImageView);
 
-            this.getChildren().add(imageView);
-
-            lastImage = currImage;
+            lastImageUrl = currImageUrl;
+            lastImageView = currImageView;
         }
     }
 
